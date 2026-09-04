de295243cab839d997e79912744a57818ac7ed20
bharanielearning-source <your-email-linked-to-bharanielearning-source>
2026-09-04 21:48:10 +0530

    day 3 update and some file from trainer

---
+++ Python/Class_603/Day-3/conditional_test_trainer.py
@@ -0,0 +1,42 @@
+isPizzaAvailable=False
+isPastaAvailable=False
+isTeaAvailable=False
+isBurgerAvailable=True
+#conditional statements if elif and else
+if(isPizzaAvailable):
+    print("buy the pizza and ingredients")
+    print("go home and make pizza")
+elif (isPastaAvailable):
+    print("buy the pasta and ingredients")
+    print("go home and make pasta")
+elif (isTeaAvailable):
+    print("buy the tea and ingredients")
+    print("go home and make tea")
+elif (isBurgerAvailable):
+    print("buy the burger and ingredients")
+    print("go home and make burger")
+else:
+    print("buy the ingredients and make maggie")
+
+
+if(1==1):
+    print("1 is equal to 1")
+if(1!=10):
+    print("1 is not equal to 10")
+if(10>5):
+    print("10 is greater than 5")
+if(10<5):
+    print("10 is less than 5")
+if(4<5):
+    print("4 is less than 5")
+if(4<=5):
+    print("4 is less than or equal to 5")
+input1=10
+input2=20
+operator="-"
+if("*"==operator):
+    print("operator is multiplication")
+    print(input1*input2)
+if("/"==operator):
+    print("operator is division")
+    print(input1/input2)
